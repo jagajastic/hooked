d2c2de8cdce535590e439e8f4d25ba60c47ea4e8
Ibrahim Joseph <josephibrahi@gmail.com>
2019-09-09 22:33:50 +0100

    Initial commit: Hook app scafolding

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../logo.svg";
+import { AuthContext } from "../App";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="card">
+        <div className="container"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
